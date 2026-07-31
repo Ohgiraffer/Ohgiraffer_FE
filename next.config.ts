4897d54c1dcbdad16a255a355ff4ee26b0d7e3ab
always62442 <always22@g.eulji.ac.kr>
2026-07-31 16:04:59 +0900

    [SETTING] 초기 파일 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   /* config options here */
+   output: 'standalone',
+   poweredByHeader: false,
+   async headers() {
+      return [
+         {
+            source: '/:path*',
+            headers: [
+               { key: 'X-Frame-Options', value: 'DENY' },
+               { key: 'X-Content-Type-Options', value: 'nosniff' },
+               { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
+            ],
+         },
+      ];
+   },
 };
 
 export default nextConfig;
