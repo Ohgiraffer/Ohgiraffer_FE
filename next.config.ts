84dbfe2a4089bcef1ab5f4142a7ceba9d7259f2a
always62442 <always22@g.eulji.ac.kr>
2026-08-15 11:57:27 +0900

    번들 분석기 설치

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
    /* config options here */
@@ -30,4 +31,10 @@ const nextConfig: NextConfig = {
    },
 };
 
-export default nextConfig;
+// 번들 분석기 설정
+const bundleAnalyzer = withBundleAnalyzer({
+   enabled: process.env.ANALYZE === 'true',
+   openAnalyzer: true,
+});
+
+export default bundleAnalyzer(nextConfig);
